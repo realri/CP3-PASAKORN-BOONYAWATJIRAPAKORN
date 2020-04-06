28025028b7bb5bf08e820f656b25dece3a884985
realri <63185389+realri@users.noreply.github.com>
2020-04-07 01:33:50 +0700

    Add files via upload

---
+++ Exercise5_1_PASAKORN_B.py
@@ -0,0 +1,6 @@
+ten = int(input("Input ten : "))
+two = int(input("Input two : "))
+print(ten,"+",two,"=", ten+two)
+print(ten,"-",two,"=", ten-two)
+print(ten,"*",two,"=", ten*two)
+print(ten,"/",two,"=", int(ten/two))
