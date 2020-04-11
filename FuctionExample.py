16c069d0fd7ba9b8aed24b6d1093ec3a1073d893
realri <63185389+realri@users.noreply.github.com>
2020-04-12 00:18:34 +0700

    Add files via upload

---
+++ FuctionExample.py
@@ -0,0 +1,15 @@
+def AddBuak(z,x):
+    print(z+x)
+AddBuak(10,10)
+
+def AddRob(a,b):
+    print(a-b)
+AddRob(10,10)
+
+def AddKhun(c,d):
+    print(c*d)
+AddKhun(10,10)
+
+def AddHan(e,f):
+    print(e/f)
+AddHan(10,10)
